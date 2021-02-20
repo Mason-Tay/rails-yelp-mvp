be09185493b12071de130ec1ba662611aa00848a
Mason Tay <iamwenzong@gmail.com>
2021-02-20 16:07:07 +0800

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+100.times do
+  category = ["Chinese", "Italian", "Japanese", "French", "Belgian"].sample
+
+  resto = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: category
+  )
+  puts resto[:name]
+  resto.save!
+end
